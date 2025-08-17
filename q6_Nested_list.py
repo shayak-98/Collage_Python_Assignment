ae4968452ac3b77e46c07cbdcdc21417e717583c
shayak-98 <sarkarshayak55@gmail.com>
2025-08-18 02:56:26 +0530

    first commit

---
+++ q6_Nested_list.py
@@ -0,0 +1,22 @@
+#Creating a nested list with user input :
+cols=int(input('Enter the no of columns'))
+rows=int(input('Enter the no of rows'))
+matrix=[]
+for i in range(rows):
+    row=[]
+    for j in range(cols):
+        val=int(input(f'Enter the {i},{j}th element'))
+        row.append(val)
+    matrix.append(row)
+print("MATRIX :")
+for i in matrix:
+    print(i)
+#Square each element in nested list :
+square_list=[]
+for i in matrix:
+    new=[]
+    for item in i:
+        new.append(item**2)
+    square_list.append(new)
+for i in square_list:
+    print(i)
